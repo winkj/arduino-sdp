525f721b388660b40d81631a25144f4036fcafbd
Johannes Winkelmann <jwi@sensirion.com>
2018-06-15 14:00:27 -0700

    fix compilation of sdp3x manual example

--- examples/sdp3x_manual_address/sdp3x_manual_address.ino
+++ examples/sdp3x_manual_address/sdp3x_manual_address.ino
@@ -5,9 +5,9 @@
 // To configure the I2C address manually, choose one of the three calls below;
 // make sure you comment out the other two, otherwise the sketch won't build!
 
-SDP3XSensor sdp(SDP3X_I2C_ADDR_21); // Address 0x21
-// SDP3XSensor sdp(SDP3X_I2C_ADDR_22); // Address 0x22
-// SDP3XSensor sdp(SDP3X_I2C_ADDR_23); // Address 0x23
+SDP3XSensor sdp(SDP3XSensor::SDP3X_I2C_ADDR_21); // Address 0x21
+// SDP3XSensor sdp(SDP3XSensor::SDP3X_I2C_ADDR_22); // Address 0x22
+// SDP3XSensor sdp(SDP3XSensor::SDP3X_I2C_ADDR_23); // Address 0x23
 
 void setup() {
   Wire.begin();
